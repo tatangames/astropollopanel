e66e746fb757537f63b7d69f547274a19fe570d2
tatangames <54828743+tatangames@users.noreply.github.com>
2023-05-24 11:25:43 -0600

    creado cron

---
+++ app/Console/Commands/VerificarOrdenes.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Ordenes;
+use App\Models\Servicios;
+use App\Models\UsuariosServicios;
+use Illuminate\Console\Command;
+use Carbon\Carbon;
+use OneSignal;
+
+
+class VerificarOrdenes extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ordenes:verificar';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'notificacion cada 1 minuto a restaurante';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+        // ENVIO DE NOTIFICACION A RESTAURANTE CADA 1 MINUTO SI LA ORDEN
+        // NO HA SIDO INICIADA O CANCELADA
+
+
+        $arrayOrdenHoy = Ordenes::where('estado_iniciada', 0)
+            ->where('estado_cancelada', 0)
+            ->whereDate('fecha_orden', '=', Carbon::today('America/El_Salvador')->toDateString())
+            ->get();
+
+        $pilaTokenRestaurante = array();
+
+        foreach ($arrayOrdenHoy as $info){
+            // obtener token del servicio que esta asociado esta orden
+
+            if($infoUsuario = UsuariosServicios::where('id_servicios', $info->id_servicio)->first()){
+                array_push($pilaTokenRestaurante, $infoUsuario->token_fcm);
+            }
+        }
+
+        if($pilaTokenRestaurante != null) {
+
+            $AppId = config('googleapi.IdApp_Restaurante');
+
+            $AppGrupoNotiPasivo = config('googleapi.IdGrupoPasivoRestaurante');
+
+            $mensaje = "Hay Nuevas Ordenes";
+            $titulo = "Revisar las ordenes Pendientes";
+
+
+            $contents = array(
+                "en" => $mensaje
+            );
+
+            $params = array(
+                'app_id' => $AppId,
+                'contents' => $contents,
+                'android_channel_id' => $AppGrupoNotiPasivo,
+                'include_player_ids' => is_array($pilaTokenRestaurante) ? $pilaTokenRestaurante : array($pilaTokenRestaurante)
+            );
+
+            $params['headings'] = array(
+                "en" => $titulo
+            );
+
+            OneSignal::sendNotificationCustom($params);
+        }
+
+
+    }
+}
