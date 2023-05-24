62fa92934d811f6d575354c678e89d0993629fd2
tatangames <54828743+tatangames@users.noreply.github.com>
2023-05-24 14:06:35 -0600

    add noti call center 2

--- app/Http/Controllers/Backend/CallCenter/CallCenterController.php
+++ app/Http/Controllers/Backend/CallCenter/CallCenterController.php
@@ -24,6 +24,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use OneSignal;
 
 class CallCenterController extends Controller
 {
